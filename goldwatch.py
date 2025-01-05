9cef67aa5514589b7f0b6664e1132cb8f59e14b8
Aaron-David-Don <aarondaviddon12@gmail.com>
2025-01-05 19:35:10 +0530

    Create goldwatch.py

---
+++ goldwatch.py
@@ -0,0 +1,106 @@
+import gradio as gr
+import asyncio
+from playwright.async_api import async_playwright
+from twilio.rest import Client
+import os
+import json
+
+async def checking(url):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page = await browser.new_page()
+        await page.goto(url)
+
+        yes = await page.locator('div.cross').is_visible()
+        if yes:
+            await page.wait_for_selector('div.cross')
+            await page.click('span.close')
+            await asyncio.sleep(2)
+        await asyncio.sleep(3)
+
+        await page.wait_for_selector('div#divSpot')
+        product_elements = await page.query_selector('div#divSpot')
+        hmm = await product_elements.query_selector('tr.product-title-text')
+        title_el = await hmm.query_selector('span.h') or await hmm.query_selector('span.e') or await hmm.query_selector('span.l')
+        sideval = float(await title_el.inner_text()) if title_el else "N/A"
+        print("The side value is: ", sideval)
+
+        await page.wait_for_selector('div#divProduct')
+        blah = await page.query_selector('div#divProduct')
+        op = await blah.query_selector('div.t1mainproduct')
+        ip = await op.query_selector_all('td.t1wth3')
+        hg = ip[1]
+        tit = await hg.query_selector('span.l.widthcls') or await hg.query_selector('span.h.widthcls') or await hg.query_selector('span.e.widthcls')
+        tabval = float(await tit.inner_text()) if tit else "N/A"
+        print("The table value is: ", tabval)
+
+        #await page.screenshot(path="screenshot.png")
+        return sideval, tabval
+
+    await browser.close()
+    
+def choks(given,taken):
+    if given == 0:
+        return False
+    else:
+        return (given<=taken)
+
+def jimmy(taken,given):
+    if given == 0:
+        return False
+    else:
+        return (taken<=given)
+
+async def scrape_amazon(url, sidel, sideh, tabl, tabh, number):
+    sideval, tabval = await checking(url)
+    while choks(sidel,sideval) or jimmy(sideval,sideh) or choks(tabl,tabval) or jimmy(tabval,tabh):
+        
+        sideval, tabval = await checking(url)
+        
+        if jimmy(sideval, sidel) or choks(sideh,sideval)  or jimmy(tabval,tabl) or choks(tabh,tabval) :
+            
+            account_sid = 'ACec0dfaf28829e1b945f06185823f88f9'
+            auth_token = '3c9b8289ec808593cd81fd784dd17a33'
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+            from_='+17065743362',
+            body=f'THE PRICE OF GOLD 1KG IS {tabval} AND THE PRICE OF GOLD SPOT IS {sideval}',
+            to=number
+            )
+
+            print(message.sid)
+    else:
+        if jimmy(sideval,sidel) or choks(sideh,sideval) or jimmy(tabval,tabl) or choks(tabh,tabval) :
+            account_sid = 'ACec0dfaf28829e1b945f06185823f88f9'
+            auth_token = '3c9b8289ec808593cd81fd784dd17a33'
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+            from_='+17065743362',
+            body=f'THE PRICE OF GOLD 1KG IS {tabval} AND THE PRICE OF GOLD SPOT IS {sideval}',
+            to=number
+            )
+            print(message.sid)
+
+# Wrapper function for Gradio interface
+def start_scraping(side_low, side_high, table_low, table_high, phone_number):
+    url = "http://www.ambicaaspot.com/liverate.html"
+    asyncio.run(scrape_amazon(url, float(side_low), float(side_high), float(table_low), float(table_high), phone_number))
+    return f"The current gold values are :\nSide Low: {side_low}, Side High: {side_high}, Table Low: {table_low}, Table High: {table_high} \nand a notification has been sent to your mobile number"
+
+# Gradio Interface
+interface = gr.Interface(
+    fn=start_scraping,
+    inputs=[
+        gr.Number(label="Side Low"),
+        gr.Number(label="Side High"),
+        gr.Number(label="Table Low"),
+        gr.Number(label="Table High"),
+        gr.Textbox(label="Phone Number (with '+')"),
+    ],
+    outputs="text",
+    title="GOLD RATE",
+    description="Kindly enter your required target values and phone number(for notification) and leave the blocks blank if not required: ",
+)
+
+# Launch the interface
+interface.launch()
